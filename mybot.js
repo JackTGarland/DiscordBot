7551b672b0be563b4867ed3dc6e7f60b22e44a43
Unknown <jacktgarland@gmail.com>
2018-10-18 13:18:34 +0100

    My first bot
    replies with Pong when ping is said in channel, also has an exit peramiter for ease of testing.

---
+++ mybot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js"); // used to get the required discord functionality
+const client = new Discord.Client();// openes a client connection with discord
+ 
+client.on("ready", () => {
+  console.log("I am ready!"); // outputs to the command line
+});
+ 
+client.on("message", (message) => { // get the message in current channel
+  if (message.content.startsWith("ping")) { // checks if that message starts with "ping"
+    message.channel.send("pong!"); // replied to message in the same channel with "Pong!"
+  }
+});
+client.on("message", (message) => {
+    if (message.content.startsWith("exit")) {
+      message.channel.send("Shutting down."); // this line is not exacuting, dont understand why.
+      console.log("shutting down!");
+      close(); // call's the close function
+    }
+  });
+ 
+function close (){
+  process.exit(1);// exits teh command line and shuts down the bot
+};
+client.login("Super secreat Token"); // login token
