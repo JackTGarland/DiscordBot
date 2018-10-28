e952d524193f12a7fed9d331b816341a5035a0fb
Unknown <jacktgarland@gmail.com>
2018-10-28 16:22:36 +0000

    Enabled new channel
    The bot can now create a new channel with the keyword "make" followed by the name you want the channel to be.

--- FaceBot.js
+++ FaceBot.js
@@ -1,14 +1,15 @@
 const Discord = require("discord.js"); // used to get the required discord functionality
 const client = new Discord.Client();// openes a client connection with discord
-const login = require("facebook-chat-api"); // FB API used to connect to the FB chat system
+//const login = require("facebook-chat-api"); // FB API used to connect to the FB chat system
 const fbMessage = require('./fbMessage.js');
+var newChannel = 0;
 
 client.on("ready", () => {
     console.log("I am ready!"); // outputs to the command line
   });
-  client.login("NTAxMDM0NTA5NDAwMjc2OTk4.DqoHiw.cwKyUPxpfY4UQ4rP17iHCzRr8YE"); // login token    
+  client.login("SuperSecreatToken"); // login token    
 
-fbLogin();
+//fbLogin();
 function fbLogin (){
 login({email: "email", password: "Password"}, (err, api) => {
     if(err) return console.error(err);
@@ -20,5 +21,23 @@ login({email: "email", password: "Password"}, (err, api) => {
 };
 
 client.on("message", (message) => { // get the message in current channel
-    fbMessage.reply(message);
+   newChannel = fbMessage.reply(message);
+   console.log(newChannel);
+   if (newChannel == 1){
+       console.log("creating new cahnnel");
+        var name = message.content;
+        name = name.substr(name.indexOf(' ')+1);
+        fbMessage.makeNewChannel(message, name);
+        makeNewChannel = 0;
+   };
+  });
+  function close (){
+    process.exit(1);// exits teh command line and shuts down the bot
+  };  
+  client.on("message", (message) => {
+    if (message.content.startsWith("exit")) {
+      message.channel.send("Shutting down."); // this line is not exacuting, dont understand why.
+      console.log("shutting down!");
+      close(); // call's the close function
+    }
   });
--- fbMessage.js
+++ fbMessage.js
@@ -2,7 +2,21 @@
 
 exports.reply = function readMessage(message){
     if (message.content.startsWith("ping")) { // checks if that message starts with "ping"
+    console.log("heard ping");
       message.channel.send("pong!"); // replied to message in the same channel with "Pong!"
+      return 0;
+    }
+    else if(message.content.startsWith("make")){
+      console.log("make heard");
+      //console.log(message.content);
+      return 1;
+     
     }
 };
 
+exports.makeNewChannel = function createNewChannel(message , name){
+  console.log("function called)");
+  //var server = message.guild;
+  message.guild.createChannel(name, "text");
+  //server.createChannel(name, "texxt");
+};
