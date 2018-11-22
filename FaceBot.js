7ff47a3e73448dda729f3b65deddae2b4ae42330
Shaun Porter <shaunoman840@gmail.com>
2018-11-22 19:57:39 +0000

    NPM initalization and token security
    Initalized project with npm
    
    Set tokens in seprate ignored js file.

--- FaceBot.js
+++ FaceBot.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js"); // General Discord functionality module
 //const login = require("facebook-chat-api"); // FaceBook API module
 const fbMessage = require('./fbMessage.js');
+const tokens = require("./token.js");
 const client = new Discord.Client(); // Creates an instance of discord.js. Used to establish a connection to the discord service
 
 // Initalization
 var newChannel = 0;
-client.login("NTAxMDM0NTA5NDAwMjc2OTk4.DqoHiw.cwKyUPxpfY4UQ4rP17iHCzRr8YE"); // login token  
+client.login(tokens.discordToken); // login token  
 
 client.on("ready", () => {
   console.log("I am ready!");
